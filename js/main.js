6f75653a4e57dfba9bee5df86b3627517fb5aae7
山村勇貴 <yamamurayuuki@yamamurayuuki-no-MacBook-Pro.local>
2020-09-30 09:23:22 +0900

    レイアウト調整

--- js/main.js
+++ js/main.js
@@ -11,16 +11,18 @@ $(function(){
   $('#nav-toggle').on('click', function(){
     $('.wrap').toggleClass('open');
   });
+  $('#gloval-nav a').on('click', function() {
+        $('.wrap').removeClass('open');
+    });
 }());
 
 // 見出し下線
 $(window).on('scroll',function(){
-
-  $(".JS_ScrollAnimationItem").each(function(){
+  $(".scrollanimation").each(function(){
     var position = $(this).offset().top;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
-    if (scroll > position - windowHeight + 140){
+    if (scroll > position - windowHeight + 180){
       $(this).addClass('isActive');
     }
   });
